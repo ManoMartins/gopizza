ceb03c5b1d8369c21764417e9f9de7f031c24cd1
Manoel Martins <mano.martins29@gmail.com>
2022-01-01 06:14:43 -0300

    :sparkles: add theme provider

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import theme from '@src/theme';
+import { Text } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar'; 
+import { ThemeProvider } from 'styled-components/native';
 import { useFonts,  DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
-import AppLoading from 'expo-app-loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,9 +18,9 @@ export default function App() {
   } 
 
   return (
-    <View>
+    <ThemeProvider  theme={theme}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </ThemeProvider>
   );
 }
