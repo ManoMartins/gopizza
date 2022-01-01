eb563d6a1ae1e1da29f288ff39f589931f05ff43
Manoel Martins <mano.martins29@gmail.com>
2022-01-01 06:04:35 -0300

    :tada: first commit

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+          alias: {
+            '@components': './src/components',
+            '@assets': './src/assets',
+            '@screens': './src/screens',
+          }
+        }
+      ]
+    ]
+  };
+};
